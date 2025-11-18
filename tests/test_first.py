0bc4e4adaaad9efb2e736ed2a215ced54444ba7a
maneshkadam67 <maneshkadam67@gmail.com>
2025-11-18 14:42:31 +0530

    this is first commit

---
+++ tests/test_first.py
@@ -0,0 +1,15 @@
+
+import pytest
+
+class Test_Testcases:
+    def test_first_testcase(self):
+        print("test_first_testcase----------------test_first_testcase")
+
+    def test_second_testcase(self):
+        print("test_second_testcase----------------test_second_testcase")
+
+    def test_third_testcase(self):
+        print("test_third_testcase-----------------test_third_testcase")
+
+    def test_fourth_testcase(self):
+        print("test_fourth_testcase-----------------test_fourth_testcase")
